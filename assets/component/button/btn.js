1277cb29caf29a9a8490a7826ca4f445d52f4e63
Hector <hector@artbees.net>
2018-11-22 13:20:14 +0330

    add js

--- assets/component/button/btn.js
+++ assets/component/button/btn.js
@@ -1,5 +1,5 @@
 
-function btn(text,color) {
-    if(!color) color="red-color";
-    return '<button class="btn +'color'">'+text+'</button>';
+function btn(text, color) {
+    if (!color) color = "red-color";
+    return '<button class="btn +' + color + '">' + text + '</button>';
 }
---
+++ assets/component/dialogue/dialogue.js
@@ -0,0 +1,6 @@
+function dialogue(header, body) {
+    return '<div class="dialogue"><h1>' + header + '</h1><p>' + body + '</p></div>';
+}
+function dialogue_html(html) {
+    return '<div class="dialogue">' + html + '</div>';
+}
