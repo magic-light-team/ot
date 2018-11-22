56470c81663c9f1c71312ceb76d92f06dcf02305
Hector <6560172+h71@users.noreply.github.com>
2018-11-22 08:33:29 +0330

    Audio

---
+++ ot.js
@@ -0,0 +1,1 @@
+// let tapSFX = new sound('tap.wav');
---
+++ sound.js
@@ -0,0 +1,17 @@
+function sound(src) {
+    this.sound = document.createElement('audio');
+    this.sound.src =  './assets/audio/'+ src;
+    this.sound.setAttribute('preload', 'auto');
+    this.sound.setAttribute('controls', 'none');
+	this.sound.style.display = 'none';
+
+	document.body.appendChild(this.sound);
+
+    this.play = function(){
+        this.sound.play();
+	}
+
+    this.stop = function(){
+        this.sound.pause();
+    }
+}
