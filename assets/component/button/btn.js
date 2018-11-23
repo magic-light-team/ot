1060d37cc0b2bf78fac91529ec75e9638698cdf2
Hector <6560172+h71@users.noreply.github.com>
2018-11-23 14:26:16 +0330

    BTNs

--- assets/component/button/btn.js
+++ assets/component/button/btn.js
@@ -1,5 +1,5 @@
 
 function btn(text, color) {
     if (!color) color = "red-color";
-    return '<button class="btn +' + color + '">' + text + '</button>';
+    return '<button class="btn ' + color + '">' + text + '</button>';
 }
