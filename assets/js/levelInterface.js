8e0ca62e5189aa04809bef8fbc58d6cff62740f1
Hector <hector@artbees.net>
2018-11-23 09:05:00 +0330

    remove js

--- assets/js/levelInterface.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var SectionType;
-(function (SectionType) {
-    SectionType[SectionType["dialog"] = 0] = "dialog";
-    SectionType[SectionType["choose"] = 1] = "choose";
-})(SectionType = exports.SectionType || (exports.SectionType = {}));
