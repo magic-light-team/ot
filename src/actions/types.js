7db90808dda8560c1e339a4f91bd63e6e2c9f20d
mohsen12999 <mohsen1299@gmail.com>
2018-12-25 02:08:04 +0330

    fix bug type

--- src/actions/types.js
+++ src/actions/types.js
@@ -5,7 +5,7 @@ export const START_PAGE   = 'START_PAGE';
 export const CHAPTER_PAGE = 'CHAPTER_PAGE';
 export const GAME_PAGE    = 'GAME_PAGE';
 export const RESUME_PAGE  = 'RESUME_PAGE';
-export const SCORE_PAGE   = 'GASCOREAGE';
+export const SCORE_PAGE   = 'SCORE_PAGE'; //'GASCOREAGE';
 
 export const CHANGE_PAGE   = 'CHANGE_PAGE';
 export const CHANGE_STAGE  = 'CHANGE_STAGE';
