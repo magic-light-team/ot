9738c5c7f59a4885619dbadc51b131baf9cab717
mohsen shabanian <mohsen1299@gmail.com>
2019-04-21 22:15:08 +0430

    making source page temp

--- src/components/aboutPage.jsx
+++ src/components/aboutPage.jsx
@@ -10,46 +10,46 @@ class AboutPage extends Component {
     render() {
         return (
             <div id="about-page" style={{ backgroundImage: 'url(' + process.env.PUBLIC_URL + "/img/levels/s1-1.jpg" + ')' }}>
-                <div class="header-info">
+                <div className="header-info">
                     <div><img src={process.env.PUBLIC_URL + "/img/about/mglogo.png"} alt="Magic Light logo" /></div>
                     <h4> تیم توسعه بازی </h4>
                     <p><a href="http://magiclight.ir">سایت استدیو مجیک لایت</a></p>
                     <p> معرفی اعضای تیم مجیک لایت </p>
-                    <div id="about-page-back-btn" onClick="changePage('Start-Page')"><h1>&gt;&gt;</h1><div></div></div>
+                    <div id="about-page-back-btn" onClick={event => this.props.changePage(event,"start-page") }><h1>&gt;&gt;</h1></div>
                 </div>
                 <div className="body">
 
-                    <div class="team-card">
-                        <div class="col">
-                            <div class="card">
-                                <img class="card-img-top" src={process.env.PUBLIC_URL + "/img/about/amir.jpg"} alt="Amir Salar Soleymani" />
-                                <div class="card-body">
-                                    <h3 class="card-title"> امیر سالار سلیمانی </h3>
-                                    <p class="card-text"> مدیر تیم و طراح بازی </p>
+                    <div className="team-card">
+                        <div className="col">
+                            <div className="card">
+                                <img className="card-img-top" src={process.env.PUBLIC_URL + "/img/about/amir.jpg"} alt="Amir Salar Soleymani" />
+                                <div className="card-body">
+                                    <h3 className="card-title"> امیر سالار سلیمانی </h3>
+                                    <p className="card-text"> مدیر تیم و طراح بازی </p>
                                 </div>
                             </div>
                         </div>
                     </div>
 
-                    <div class="team-card">
-                        <div class="col">
-                            <div class="card">
-                                <img class="card-img-top" src={process.env.PUBLIC_URL + "/img/about/hosein.jpg"} alt="Hosein Farahani" />
-                                <div class="card-body">
-                                    <h3 class="card-title"> حسین فراهانی </h3>
-                                    <p class="card-text"> برنامه نویس </p>
+                    <div className="team-card">
+                        <div className="col">
+                            <div className="card">
+                                <img className="card-img-top" src={process.env.PUBLIC_URL + "/img/about/hosein.jpg"} alt="Hosein Farahani" />
+                                <div className="card-body">
+                                    <h3 className="card-title"> حسین فراهانی </h3>
+                                    <p className="card-text"> برنامه نویس </p>
                                 </div>
                             </div>
                         </div>
                     </div>
 
-                    <div class="team-card">
-                        <div class="col">
-                            <div class="card">
-                                <img class="card-img-top" src={process.env.PUBLIC_URL + "/img/about/mohsen.jpg"} alt="Mohsen Shabanian" />
-                                <div class="card-body">
-                                    <h3 class="card-title"> محسن شعبانیان </h3>
-                                    <p class="card-text"> برنامه نویس </p>
+                    <div className="team-card">
+                        <div className="col">
+                            <div className="card">
+                                <img className="card-img-top" src={process.env.PUBLIC_URL + "/img/about/mohsen.jpg"} alt="Mohsen Shabanian" />
+                                <div className="card-body">
+                                    <h3 className="card-title"> محسن شعبانیان </h3>
+                                    <p className="card-text"> برنامه نویس </p>
                                 </div>
                             </div>
                         </div>
