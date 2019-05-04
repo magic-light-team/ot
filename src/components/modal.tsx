5c9a7827512909c2f7af178619407d11087eb68d
mohsen shabanian <mohsen1299@gmail.com>
2019-05-04 11:42:57 +0430

    add modal

---
+++ src/components/modal.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import './modal.css'
+import BigButton from './bigbutton';
+
+// const Modal = ({ pauseGame, changePage }) => {
+//     return (
+//         <div className="dimmer" onClick={event => pauseGame(event,false) }>
+//             <div className="modal">
+//                 <h1>منو</h1>
+//                 <BigButton id="1" text="ادامه بازی"  arg="" clickHandle={pauseGame} color="" />
+//                 <BigButton id="2" text="شروع دوباره" arg="chapterPage" clickHandle={changePage} color="" />
+//                 <BigButton id="3" text="منوی بازی"   arg="startPage" clickHandle={changePage} color="" />
+//             </div>
+//         </div>
+//     );
+// };
+
+interface ModalProps {
+    pauseGame: Function,
+    changePage: Function,
+}
+
+const Modal: React.FC<ModalProps> = ({ pauseGame, changePage }) => {
+    return (
+        <div className="dimmer" onClick={event => pauseGame(event, false)}>
+            <div className="modal">
+                <h1>منو</h1>
+                <BigButton id="1" text="ادامه بازی" arg="" clickHandle={pauseGame} color="" />
+                <BigButton id="2" text="شروع دوباره" arg="chapterPage" clickHandle={changePage} color="" />
+                <BigButton id="3" text="منوی بازی" arg="startPage" clickHandle={changePage} color="" />
+            </div>
+        </div>
+    );
+};
+
+export default Modal;
