54a988520f9e1f66e5e01ef2a9e4165396a41962
mohsen shabanian <mohsen1299@gmail.com>
2019-05-05 20:48:30 +0430

    Updates

---
+++ precache-manifest.2ed956fdd28c42d0a2bdb5238d5ae4ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f80564b68e09afafa2a4c2a22e45205a",
+    "url": "/ot/index.html"
+  },
+  {
+    "revision": "b21864156424ee2ef70a",
+    "url": "/ot/static/css/main.ced2529e.chunk.css"
+  },
+  {
+    "revision": "4b4a79e566edc8e5e8dd",
+    "url": "/ot/static/js/2.8aed6ec8.chunk.js"
+  },
+  {
+    "revision": "b21864156424ee2ef70a",
+    "url": "/ot/static/js/main.248ac29c.chunk.js"
+  },
+  {
+    "revision": "398dfa72f3f1ebb9f128",
+    "url": "/ot/static/js/runtime~main.5d251d3c.js"
+  }
+]);
