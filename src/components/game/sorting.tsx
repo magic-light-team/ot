26c4cbde21b138f04800bb83085766e7a05999e2
mohsen shabanian <mohsen1299@gmail.com>
2019-05-11 13:34:58 +0430

    make map for sorting

--- src/components/game/sorting.tsx
+++ src/components/game/sorting.tsx
@@ -4,18 +4,32 @@ import './sorting.css'
 import { DragDropContextProvider } from 'react-dnd';
 import HTML5Backend from 'react-dnd-html5-backend';
 import Card from './card';
+import { option } from '../../info/data.interfaces';
 
 export interface SortingProps {
   // stageDialog:string,
   // dialogType?:DialogType,
+  options?: option[],
+  score?:number,
 }
 
 const style = {
   width: 400,
 }
 
-const Sorting: React.FC<SortingProps> = ({ }) => {
+// interface ICard { index: number; text: string };
+// class cardClass implements ICard{
+class cardClass{
+  constructor(index: number, text: string){
+    this.index=index;
+    this.text=text;
+  }
+  index: number;
+  text: string;
+}
 
+const Sorting: React.FC<SortingProps> = ({options }) => {
+  // const[cards2,setCards2] = useState(options.map((opt,i)=> new cardClass(i,opt.title)))
   const [cards, setCards] = useState([
     {
       id: 1,
@@ -55,9 +69,24 @@ const Sorting: React.FC<SortingProps> = ({ }) => {
     setCards(newCards);
   }
 
+  const checkCards = () =>{
+    let isSorted = true;
+    for (let index = 0; index < cards.length-1; index++) {
+      if(cards[index].id>cards[index].id)
+      {
+        isSorted=false;
+        break;
+      }
+    }
+
+    //
+  }
+
   return (
     <div>
-      <div className="question_dialoug"></div>
+      <div className={"description-wrapper dialog "} style={{ backgroundImage: 'url(' + process.env.PUBLIC_URL + '/img/dialog/left.png)' }}>
+                مرتب کنید</div>
+      {/* <div className="question_dialoug"></div> */}
       <DragDropContextProvider backend={HTML5Backend}>
         <div style={style}>
           {cards.map((card, i) => (
@@ -73,7 +102,7 @@ const Sorting: React.FC<SortingProps> = ({ }) => {
       </DragDropContextProvider>
       <div className="btn-wrapper">
         <button
-          // onClick={event => }
+          onClick={checkCards}
           className="btn red-color">بررسی جواب</button>
       </div>
     </div>
